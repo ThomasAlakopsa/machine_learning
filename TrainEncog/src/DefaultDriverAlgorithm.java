8bb5459ee701fcf37da19cb231a88592e7a36f62
alexderidder <alex.deridder@hotmail.com>
2019-10-16 10:53:33 +0200

    Added Neural Network with Encog

--- TrainEncog/src/DefaultDriverAlgorithm.java
+++ TrainEncog/src/DefaultDriverAlgorithm.java
@@ -1,10 +1,8 @@
 import java.io.File;
 
 import cicontest.algorithm.abstracts.AbstractAlgorithm;
-import cicontest.algorithm.abstracts.AbstractRace;
 import cicontest.algorithm.abstracts.DriversUtils;
 import cicontest.torcs.controller.Driver;
-import cicontest.torcs.controller.Human;
 import race.TorcsConfiguration;
 
 public class DefaultDriverAlgorithm extends AbstractAlgorithm {
@@ -44,24 +42,16 @@ public class DefaultDriverAlgorithm extends AbstractAlgorithm {
 
         //Set path to torcs.properties
         TorcsConfiguration.getInstance().initialize(new File("torcs.properties"));
-        /*
-		 *
-		 * Start without arguments to run the algorithm
-		 * Start with -continue to continue a previous run
-		 * Start with -show to show the best found
-		 * Start with -show-race to show a race with 10 copies of the best found
-		 * Start with -human to race against the best found
-		 *
-		 */
+
         DefaultDriverAlgorithm algorithm = new DefaultDriverAlgorithm();
         DriversUtils.registerMemory(algorithm.getDriverClass());
-        if (args.length > 0 && args[0].equals("-show")) {
-            new DefaultRace().showBest();
-        } else if (args.length > 0 && args[0].equals("-show-race")) {
-            new DefaultRace().showBestRace();
-        } else if (args.length > 0 && args[0].equals("-human")) {
-            new DefaultRace().raceBest();
-        } else if (args.length > 0 && args[0].equals("-continue")) {
+        if (args.length > 0 && args[0].equals("-resumeTraining")) {
+            NeuralNetwork net = new NeuralNetwork();
+            net.train(true);
+        } else if (args.length > 0 && args[0].equals("-createTraining")) {
+            NeuralNetwork net = new NeuralNetwork(22, 100, 40, 3);
+            net.train(false);
+        } else if (args.length > 0 && args[0].equals("-run")) {
             if (DriversUtils.hasCheckpoint()) {
                 DriversUtils.loadCheckpoint().run(true);
             } else {
