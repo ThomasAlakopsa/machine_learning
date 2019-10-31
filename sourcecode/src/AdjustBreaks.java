58713e91a56ddd7b239fe654759b26cd172b83a5
alexderidder <alex.deridder@hotmail.com>
2019-10-31 14:33:53 +0100

    Add jar controller

--- sourcecode/src/AdjustBreaks.java
+++ sourcecode/src/AdjustBreaks.java
@@ -7,16 +7,19 @@ public class AdjustBreaks implements IExtra {
     int stuck = 0;
     @Override
     public void process(Action action, SensorModel sensors) {
-
+        //Probleem dat Neurale Netwerk moeilijk start vanaf het begin van de race
         if (sensors.getSpeed() <= 50){
             stuck++;
+            //Controle of de auto stil staat
             if (stuck <= 100){
+                //Auto heeft moeite met wegrijden, override de brake van het neurale netwerk
                 action.brake = 0;
                 return;
             }
         }
         if (stuck > 100){
             action.brake = 0;
+            //Auto rijdt weer, brake van neurale netwerk gebruiken
             if(sensors.getSpeed() > 60){
                 stuck = 0;
             }
@@ -30,4 +33,5 @@ public class AdjustBreaks implements IExtra {
 
     }
 }
+
 
